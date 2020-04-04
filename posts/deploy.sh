a7adb404be2f8474d738349e7f4b3c7abf960532
zsept <zseptspace@163.com>
2020-04-04 19:10:11 +0800

    update

---
+++ posts/deploy.sh
@@ -0,0 +1,30 @@
+# copy data
+cp -rf /home/z/Workspace/znote/*  /home/z/Workspace/zblog/zblog_hugo/content/posts/
+
+rm -rf /home/z/Workspace/zblog/zblog_hugo/content/deploy.sh
+
+
+
+# delete old 
+rm -rf /home/z/Workspace/zblog/zblog_hugo/public
+
+
+
+# push 
+cd /home/z/Workspace/zblog/zblog_hugo
+
+hugo 
+
+cp -rf /home/z/Workspace/zblog/zblog_hugo/public/.  /home/z/Workspace/zblog/zsept.github.io/
+cp -rf /home/z/Workspace/zblog/zblog_hugo/CNAME  /home/z/Workspace/zblog/zsept.github.io/CNAME
+cp -rf /home/z/Workspace/zblog/zblog_hugo/README.md  /home/z/Workspace/zblog/zsept.github.io/README.md
+
+cd /home/z/Workspace/zblog/zsept.github.io
+
+git add .
+git commit -m "update"
+git push
+
+
+
+
